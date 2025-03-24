4813a76f83abbf50e0db9149d96f8324902013ba
kaitokosuge <kaitokosuge.mi@gmail.com>
2025-03-24 11:38:01 +0900

    add: if handle

--- if-handle/src/main.rs
+++ if-handle/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let a = 3;
+
+    if a < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    absolute_zero()
+}
+
+fn absolute_zero() {
+    let a = 3;
+    if a == 3 {
+        println!("three!")
+    }
 }
